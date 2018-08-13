6e7d06051e40956ae3cf6cd4dd9e057504cf135b
吴涛 <1031012833@qq.com>
2018-08-13 09:29:57 +0800

    基础部分done

--- src/fib.go
+++ src/fib.go
@@ -1,17 +1,37 @@
 //斐波那契数列第n个数
+//package main
+//
+//import "fmt"
+//
+//func fib(n int) int {
+//	x, y := 0 ,1
+//	for i := 0; i < n; i++ {
+//		x, y = y, x+y
+//	}
+//	return x
+//}
+//
+//func main() {
+//	ret := fib(10)
+//	fmt.Println(ret)
+//}
+
+
+
 package main
 
 import "fmt"
 
-func fib(n int) int {
-	x, y := 0 ,1
-	for i := 0; i < n; i++ {
-		x, y = y, x+y
+func fibonacci(n int) int {
+	if n < 2 {
+		return n
 	}
-	return x
+	return fibonacci(n-2) + fibonacci(n-1)
 }
 
 func main() {
-	ret := fib(10)
-	fmt.Println(ret)
+	var i int
+	for i = 0; i < 10; i++ {
+		fmt.Printf("%d\t", fibonacci(i))
+	}
 }
