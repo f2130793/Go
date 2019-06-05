926a8d9d1474625c58c59430659dfbbdca864572
莫轩然 <1031012833@qq.com>
2019-06-05 11:00:13 +0800

    1

---
+++ src/chan.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func counter(out chan<- int) {
+	for i := 0; i < 100; i++ {
+		out <- i
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer1(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go counter(ch1)
+	go squarer(ch2, ch1)
+	printer1(ch2)
+}
